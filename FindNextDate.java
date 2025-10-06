6dfe420bb0457dc8b2afc616b22f899073868d5e
abhyudaydubey4 <116032708+abhyudaydubey4@users.noreply.github.com>
2025-10-06 23:34:10 +0530

    Create FindNextDate.java

---
+++ FindNextDate.java
@@ -0,0 +1,15 @@
+public class FindNextDate {
+  public static void main(String []args) {
+    Scanner scn = new Scanner(System.in);
+    System.out.println("Enter today's Date : " );
+    int currentDate = scn.nextInt();
+    System.out.println (" Calculating Next Date...");
+    System.out.println ("Analysing input with AI..");
+    System.out.println ("Feeding to latest LLM...");
+
+    nextDate = 1;
+    if( currentDate + 1 <= 31 )
+      nextDate = currentDate + 1;
+    System.out.printn( "Next Date is found and is = " + nextDate);
+  }
+}
