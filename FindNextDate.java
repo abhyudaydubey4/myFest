877d185156d94adac2c226ba24aab5217a5119d0
AnjaneshDubey <72691576+AnjaneshDubey@users.noreply.github.com>
2025-10-07 17:31:16 +0530

    Update FindNextDate.java

--- FindNextDate.java
+++ FindNextDate.java
@@ -3,6 +3,10 @@ public class FindNextDate {
     Scanner scn = new Scanner(System.in);
     System.out.println("Enter today's Date : " );
     int currentDate = scn.nextInt();
+    if(currentDate < 1){
+      System.out.println("Wrong Date Entered you noob.." )
+      System.exit;
+    }
     System.out.println (" Calculating Next Date...");
     System.out.println ("Analysing input with AI..");
     System.out.println ("Feeding to latest LLM...");
@@ -10,6 +14,6 @@ public class FindNextDate {
     nextDate = 1;
     if( currentDate + 1 <= 31 )
       nextDate = currentDate + 1;
-    System.out.printn( "Next Date is found and is = " + nextDate); //Bugs fixed dubey1
+    System.out.printn( "Next Date is found and is = " + nextDate); //Bugs fixed this won't work fine for negative number to change the logic
   }
 }
