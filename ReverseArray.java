fb8286e10b6e28ae0f9b80a0a2bd62f6045850e8
saumill <terminatorbeast47@gmail.com>
2025-10-18 17:16:35 +0530

    Update ReverseArray.java

--- ReverseArray.java
+++ ReverseArray.java
@@ -10,7 +10,7 @@
             System.out.println("Array in reverse order: ");  
             //Loop through the array in reverse order  
             for (int i = arr.length-1; i >= 0; i--) {  //Update 2 reversing can be done without this method as well
-                System.out.print(arr[i] + " ");  //This is working code nicely done.
+                System.out.print(arr[i] + " ");  //This is working code.
             }  
         }  
     }  
